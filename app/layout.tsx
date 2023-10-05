9ac2df49968ea91769955a06650f34344aedcd46
longfeiwang <erwangfew@gmail.com>
2023-10-05 14:57:43 -0700

    feat: ✨ final

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ToasterProvider } from '@/components/toaster-provider'
 import { ModalProvider } from '@/components/modal-provider'
 import { CrispProvider } from '@/components/crisp-provider'
+import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -22,9 +23,16 @@ export default function RootLayout({
       <html lang="en">
         <CrispProvider />
         <body className={inter.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={true}
+            storageKey='ai-saas'>
+            <ToasterProvider />
+            <ModalProvider />
+
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import { UserButton } from "@clerk/nextjs"
 import MobileSidebar from './mobile-sidebar'
 import { getApiLimitCount } from "@/lib/api-limit"
 import { checkSubscription } from "@/lib/subscription";
+import { ModeToggle } from "./moddle-toggle";
 
 const Navbar = async () => {
     const apiLimitCount = await getApiLimitCount()
@@ -9,7 +10,8 @@ const Navbar = async () => {
     return (
         <div className='flex items-center p-4'>
             <MobileSidebar apiLimitCount={apiLimitCount} isPro={isPro} />
-            <div className='flex w-full justify-end'>
+            <div className='flex w-full justify-between'>
+                <ModeToggle />
                 <UserButton afterSignOutUrl="/" />
             </div>
         </div>
